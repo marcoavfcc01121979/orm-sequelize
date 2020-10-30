96db767c66430e9db9e4f1a158c608bcede4a085
marcoavfcc01121979 <marcoavfcc@gmail.com>
2020-10-30 16:39:21 -0300

    Initial orm sequelize alura

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json())
+
+const port = 3000;
+
+app.get('/teste', (req,res) => res
+  .status(200)
+  .send({ mensagem: 'boas vindas á API' })
+)
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
+
+module.exports = app;
